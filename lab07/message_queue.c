7df6f13de0a39500480c681a255747a7f253f00b
abubaca4 <abubaca4@gmail.com>
2020-05-17 20:18:17 +0300

    7 progress

--- lab07/message_queue.c
+++ lab07/message_queue.c
@@ -1,79 +1,79 @@
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <sys/ipc.h> 
-#include <sys/msg.h>  
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-struct Strmsg 
+int get_random_number(int min, int max)
 {
-    long mtype;
-    char str[256];     
-    int islast; 
-};  
-
-void parentMainCode(int msgId) 
-{     
-    struct Strmsg localmsg;     
-    int i;          
-    printf("Parent: 15 messages will be send.\n");     
-    for (i = 1; i <= 15; ++i)     
-    {         
-        //prepare message         
-        localmsg.mtype = 7;         
-        sprintf(localmsg.str, "this is message number %i", i);         
-        localmsg.islast = !(i < 15);                  
-        //send message         
-        msgsnd(msgId, &localmsg, sizeof(localmsg), 0);     
-    }          
-    printf("Parent: wait until child is finished.\n");     
-    waitpid(0, 0, 0);          
-    printf("Parent: releasing the message queue.\n");     
-    msgctl(msgId, IPC_RMID, NULL);          
-    printf("Parent: Process is finished.\n"); 
+    return rand() % (max - min + 1) + min;
 }
 
-void childMainCode(int msgId) 
-{     
-    struct Strmsg childlocalmsg;      
-    int islast = 0;     
-    do     
-    {         
-        //read next message         
-        ssize_t len = msgrcv(msgId, &childlocalmsg, sizeof(childlocalmsg), 0, 0);         
-        printf("Child: message with len = %i incoming.\n", len);         
-        islast = childlocalmsg.islast;         
-        printf("Child: the following string was received: %s\n", childlocalmsg.str);     
-    } 
-    while(!islast);          
-    printf("Child: the last message was read.\n");     
-    printf("Child: Process is finished.\n"); 
-}  
+struct Strmsg
+{
+    long mtype;
+    int data[4];
+    int islast;
+};
 
-int main() 
-{     
-    const size_t semCount = 10;     
-    int msgId = msgget(IPC_PRIVATE, 0600|IPC_CREAT);     
-    if (msgId < 0)     
-    {         
-        perror("error with msgget()");         
-        return -1;     
-    }     
-    else     
-    {         
-        printf("message id = %i\n", msgId);     
-    }      
-    pid_t childId = fork();     
-    if (childId < 0)     
-    {         
-        perror("error with fork()\n");     
-    }     
-    else 
-        if (childId > 0)     
-        {         
-            parentMainCode(msgId);     
-        }     
-        else    
-        {         
-            childMainCode(msgId);     
-        }          
-    return 0; 
+void parentMainCode(int msgId)
+{
+    struct Strmsg localmsg;
+    for (int i = 0; i < 4; i++)
+        local_msg.data[i] = get_random_number(0, 10000);
+    localmsg.islast = 1;
+    localmsg.mtype = 1;
+    msgsnd(msgId, localmsg, sizeof(localmsg), 0);
+    int count_of_r = 0;
+    do
+    {
+        msgrcv(msgId, localmsg, sizeof(localmsg), 2, 0);
+        count_of_r = count_of_r + 1;
+        printf("Parent get %i: %i %i %i %i", count_of_r, local_msg.data[0], local_msg.data[1], local_msg.data[2], local_msg.data[3]);
+    } while (!localmsg.islast);
+    printf("Parent: wait until child is finished.\n");
+    waitpid(0, 0, 0);
+    printf("Parent: releasing the message queue.\n");
+    msgctl(msgId, IPC_RMID, NULL);
+    printf("Parent: Process is finished.\n");
+}
+
+void childMainCode(int msgId)
+{
+    struct Strmsg childlocalmsg;
+    msgrcv(msgId, childlocalmsg, sizeof(childlocalmsg), 1, 0);
+    int data[4];
+    for (int i = 0; i < 3; i++)
+        data[i] = childlocalmsg.data[i];
+    
+    printf("Child: the last message was read.\n");
+    printf("Child: Process is finished.\n");
+}
+
+int main()
+{
+    const size_t semCount = 10;
+    int msgId = msgget(IPC_PRIVATE, 0600 | IPC_CREAT);
+    if (msgId < 0)
+    {
+        perror("error with msgget()");
+        return -1;
+    }
+    else
+    {
+        printf("message id = %i\n", msgId);
+    }
+    pid_t childId = fork();
+    if (childId < 0)
+    {
+        perror("error with fork()\n");
+    }
+    else if (childId > 0)
+    {
+        parentMainCode(msgId);
+    }
+    else
+    {
+        childMainCode(msgId);
+    }
+    return 0;
 }
