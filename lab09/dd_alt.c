631d09d8351d75e230af079a2bc24bbd6c6c55d9
abubaca4 <abubaca4@gmail.com>
2020-05-18 01:27:57 +0300

    fix

--- lab09/dd_alt.c
+++ lab09/dd_alt.c
@@ -10,6 +10,7 @@ int main(int argv, char *argc[])
 
 #define input_file_name argc[1]
     int byte_count = atoi(argc[2]);
+    printf("%d\n", byte_count);
 
     FILE *fin = fopen(input_file_name, "r");
     if (NULL == fin)
