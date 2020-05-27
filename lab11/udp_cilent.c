19f33c8ce0c24577d1307bd935f60efae3687358
abubaca4 <abubaca4@gmail.com>
2020-05-27 18:09:04 +0300

    Разбитие на пакеты для больших посылок готово

--- lab11/udp_cilent.c
+++ lab11/udp_cilent.c
@@ -1,13 +1,13 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <malloc.h>
 #include <sys/time.h>
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 int get_random_number(int min, int max)
 {
@@ -16,17 +16,17 @@ int get_random_number(int min, int max)
 
 unsigned long long mic_s_time()
 {
-  struct timeval t;
-  gettimeofday(&t, NULL);
-  unsigned long long mt = (unsigned long long)t.tv_sec * 1000000 + t.tv_usec;
-  return mt;
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    unsigned long long mt = (unsigned long long)t.tv_sec * 1000000 + t.tv_usec;
+    return mt;
 }
-  
+
 #define MAX_SAVE_UDP_DATA_SIZE 508
 
-// Driver code 
-int main(int argv, char *argc[]) 
-{ 
+// Driver code
+int main(int argv, char *argc[])
+{
     int MAS_SIZE = 10, MIN_R = 0, MAX_R = 1000;
     if (argv != 2)
     {
@@ -56,93 +56,100 @@ int main(int argv, char *argc[])
     ip[border] = '\0';
     int port = atoi(argc[1] + border + 1);
 
-    int sockfd; 
-    int *buffer;  
-    struct sockaddr_in     servaddr; 
-  
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-  
-    memset(&servaddr, 0, sizeof(servaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(port); 
+    int sockfd;
+    int *buffer;
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
     inet_aton(ip, &(servaddr.sin_addr));
-      
-    int len; 
+
+    int len;
     unsigned long long t;
 
-    sendto(sockfd, (int *)&MAS_SIZE, sizeof(MAS_SIZE), 
-        0, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr));
+    sendto(sockfd, (int *)&MAS_SIZE, sizeof(MAS_SIZE),
+           0, (const struct sockaddr *)&servaddr,
+           sizeof(servaddr));
 
     int temp;
 
-    recvfrom(sockfd, (int *)&temp, sizeof(temp),  
-                0, (struct sockaddr *) &servaddr, 
-                &len); 
+    recvfrom(sockfd, (int *)&temp, sizeof(temp),
+             0, (struct sockaddr *)&servaddr,
+             &len);
 
     if (temp != MAS_SIZE)
     {
         perror("Size sending error");
         return -1;
     }
-    
+
     buffer = calloc(MAS_SIZE, sizeof(int));
     srand(time(NULL));
-    for (int i=0; i<MAS_SIZE; i++)
+    for (int i = 0; i < MAS_SIZE; i++)
         buffer[i] = get_random_number(MIN_R, MAX_R);
-    
-    if (MAS_SIZE * sizeof(int) <= MAX_SAVE_UDP_DATA_SIZE) 
+
+    if (MAS_SIZE * sizeof(int) <= MAX_SAVE_UDP_DATA_SIZE)
     {
-        sendto(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE, 
-            0, (const struct sockaddr *) &servaddr,  
-                sizeof(servaddr)); 
+        sendto(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,
+               0, (const struct sockaddr *)&servaddr,
+               sizeof(servaddr));
         t = mic_s_time();
-                
-        recvfrom(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,  
-                    0, (struct sockaddr *) &servaddr, 
-                    &len); 
+
+        recvfrom(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,
+                 0, (struct sockaddr *)&servaddr,
+                 &len);
         t = mic_s_time() - t;
     }
     else
     {
-        int data_tr_len = MAX_SAVE_UDP_DATA_SIZE - sizeof(int);
-        for (int i=0; i< (MAS_SIZE * sizeof(int))/data_tr_len + 1; i++)
+        int data_item_count = (MAX_SAVE_UDP_DATA_SIZE - sizeof(int)) / sizeof(int);
+        for (int i = 0; i < (MAS_SIZE / data_item_count + 1); i++)
         {
-            int packet_size = 1 + (i == MAS_SIZE * sizeof(int) / data_tr_len) ? (MAS_SIZE % (data_tr_len / sizeof(int))) : (data_tr_len / sizeof(int));
-            packet_size = packet_size * sizeof(int);
-            //Буфер и номер отправленного куска
-
-            sendto(sockfd, (int *)(buffer + data_tr_len / sizeof(int)), packet_size, 
-            0, (const struct sockaddr *) &servaddr,  
-                sizeof(servaddr));
+            int block_size = i == MAS_SIZE / data_item_count ? MAS_SIZE % data_item_count : data_item_count;
+            int data_size = sizeof(int) * (1 + block_size);
+            int *send_buf = malloc(data_size);
+            send_buf[0] = i;
+            for (int j = 1; j <= block_size; j++)
+                send_buf[j] = buffer[i * data_item_count + j - 1];
+            sendto(sockfd, (int *)send_buf, data_size,
+                   0, (const struct sockaddr *)&servaddr,
+                   sizeof(servaddr));
+            free(send_buf);
         }
         t = mic_s_time();
 
-        for (int i=0; i< (MAS_SIZE * sizeof(int))/data_tr_len + 1; i++)
+        int *recive_buf = malloc(MAX_SAVE_UDP_DATA_SIZE);
+        for (int i = 0; i < (MAS_SIZE / data_item_count + 1); i++)
         {
-            int packet_size = 1 + (i == MAS_SIZE * sizeof(int) / data_tr_len) ? (MAS_SIZE % (data_tr_len / sizeof(int))) : (data_tr_len / sizeof(int));
-            packet_size = packet_size * sizeof(int);
-
-            recvfrom(sockfd, (int *)(buffer + data_tr_len / sizeof(int)), packet_size,  
-                    0, (struct sockaddr *) &servaddr, 
-                    &len);
-            t = mic_s_time() - t; 
+            recvfrom(sockfd, (int *)recive_buf, MAX_SAVE_UDP_DATA_SIZE,
+                     0, (struct sockaddr *)&servaddr,
+                     &len);
+            int block_number = recive_buf[0];
+            int block_size = block_number == MAS_SIZE / data_item_count ? MAS_SIZE % data_item_count : data_item_count;
+            for (int j = 1; j <= block_size; j++)
+                buffer[block_number * data_item_count + j - 1] = recive_buf[j];
         }
+        free(recive_buf);
+        t = mic_s_time() - t;
     }
-    
-    printf("Server : "); 
-    for (int i=0; i<MAS_SIZE; i++)
+
+    printf("Server : ");
+    for (int i = 0; i < MAS_SIZE; i++)
         printf("%d ", buffer[i]);
-    printf("\n");   
-    printf ("It took %lld microseconds.\n",t);
-  
-    close(sockfd); 
+    printf("\n");
+    printf("It took %lld microseconds.\n", t);
+
+    close(sockfd);
     free(buffer);
-    return 0; 
-} 
+    return 0;
+}
--- lab11/udp_serv.c
+++ lab11/udp_serv.c
@@ -1,22 +1,23 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <malloc.h>
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 static int comp(const void *a, const void *b)
 {
     return *(const int *)a - *(const int *)b;
 }
-  
+
 #define MAX_SAVE_UDP_DATA_SIZE 508
 
-// Driver code 
-int main(int argv, char *argc[]) { 
+// Driver code
+int main(int argv, char *argc[])
+{
     int MAS_SIZE;
 
     if (argv != 2)
@@ -32,64 +33,100 @@ int main(int argv, char *argc[]) {
         perror("Not correct port");
         return -1;
     }
-    
-    int sockfd; 
-    int *buffer; 
-    struct sockaddr_in servaddr, cliaddr; 
-      
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-      
-    memset(&servaddr, 0, sizeof(servaddr)); 
-    memset(&cliaddr, 0, sizeof(cliaddr)); 
-      
-    // Filling server information 
-    servaddr.sin_family    = AF_INET; // IPv4 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-    servaddr.sin_port = htons(port); 
-      
-    // Bind the socket with the server address 
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
-    { 
+
+    int sockfd;
+    int *buffer;
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(port);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
         servaddr.sin_port = 0;
-        if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
+        if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
         {
-            perror("bind failed"); 
-            exit(EXIT_FAILURE); 
+            perror("bind failed");
+            exit(EXIT_FAILURE);
         }
-    } 
+    }
 
     socklen_t servlen = sizeof(servaddr);
     getsockname(sockfd, (struct sockaddr *)&servaddr, &servlen);
     printf("Lisening on port: %d\n", ntohs(servaddr.sin_port));
-      
-    int len, n; 
-  
-    len = sizeof(cliaddr);  //len is value/resuslt 
-    recvfrom(sockfd, (int *)&MAS_SIZE, sizeof(MAS_SIZE),  
-                0, (struct sockaddr *) &cliaddr, 
-                &len);
 
-    sendto(sockfd, (int *)&MAS_SIZE, sizeof(MAS_SIZE),  
-        0, (const struct sockaddr *) &cliaddr, 
-            len); 
+    int len;
+
+    len = sizeof(cliaddr); //len is value/resuslt
+    recvfrom(sockfd, (int *)&MAS_SIZE, sizeof(MAS_SIZE),
+             0, (struct sockaddr *)&cliaddr,
+             &len);
+
+    sendto(sockfd, (int *)&MAS_SIZE, sizeof(MAS_SIZE),
+           0, (const struct sockaddr *)&cliaddr,
+           len);
 
     buffer = calloc(MAS_SIZE, sizeof(int));
 
-    n = recvfrom(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,  
-                0, (struct sockaddr *) &cliaddr, 
-                &len); 
-    
-    qsort(buffer, MAS_SIZE, sizeof(buffer[0]), comp);
+    if (MAS_SIZE * sizeof(int) <= MAX_SAVE_UDP_DATA_SIZE)
+    {
+        recvfrom(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,
+                 0, (struct sockaddr *)&cliaddr,
+                 &len);
 
-    sendto(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,  
-        0, (const struct sockaddr *) &cliaddr, 
-            len); 
+        qsort(buffer, MAS_SIZE, sizeof(buffer[0]), comp);
+
+        sendto(sockfd, (int *)buffer, sizeof(buffer[0]) * MAS_SIZE,
+               0, (const struct sockaddr *)&cliaddr,
+               len);
+    }
+    else
+    {
+        int data_item_count = (MAX_SAVE_UDP_DATA_SIZE - sizeof(int)) / sizeof(int);
+        int *recive_buf = malloc(MAX_SAVE_UDP_DATA_SIZE);
+        for (int i = 0; i < (MAS_SIZE / data_item_count + 1); i++)
+        {
+            recvfrom(sockfd, (int *)recive_buf, MAX_SAVE_UDP_DATA_SIZE,
+                     0, (struct sockaddr *)&cliaddr,
+                     &len);
+            int block_number = recive_buf[0];
+            int block_size = block_number == MAS_SIZE / data_item_count ? MAS_SIZE % data_item_count : data_item_count;
+            for (int j = 1; j <= block_size; j++)
+                buffer[block_number * data_item_count + j - 1] = recive_buf[j];
+        }
+        free(recive_buf);
+
+        qsort(buffer, MAS_SIZE, sizeof(buffer[0]), comp);
+
+        for (int i = 0; i < (MAS_SIZE / data_item_count + 1); i++)
+        {
+            int block_size = i == MAS_SIZE / data_item_count ? MAS_SIZE % data_item_count : data_item_count;
+            int data_size = sizeof(int) * (1 + block_size);
+            int *send_buf = malloc(data_size);
+            send_buf[0] = i;
+            for (int j = 1; j <= block_size; j++)
+                send_buf[j] = buffer[i * data_item_count + j - 1];
+            sendto(sockfd, (int *)send_buf, data_size,
+                   0, (const struct sockaddr *)&cliaddr,
+                   len);
+            free(send_buf);
+        }
+    }
 
     free(buffer);
     printf("Server off\n");
-    return 0; 
-} 
+    return 0;
+}
